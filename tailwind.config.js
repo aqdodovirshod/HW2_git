804b6dcec137c7dacc109777b0bdef9d35099265
H4CKER <aqdodovirshod7@gmail.com>
2025-01-23 01:08:06 -0800

    Section 2(also @media for sm for sec1 sec2 header)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '375px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
